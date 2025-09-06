4e565583cb9ebcf2c14bdb9118738c9ff890dd01
Fabio William Conceição <messhias@gmail.com>
2025-09-06 16:55:49 +0100

    Bulltet system created.
    TODO: needs to figure out why the bulltet is just goes to the x position and not respecting the player facing position.

--- MasteringUnit/Assets/Scripts/PlayerController.cs
+++ MasteringUnit/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField, Tooltip("The bullet projectile prefab to fire.")]
+    private GameObject _bulletToSpawn;
+    
+    [SerializeField, Tooltip("The direction of that the Player is facing.")]
+    private Vector3 _currentFacing = Vector3.zero;
+    
     // the rigid body physics component of this object
     // since we'll be accessing it a lot, we'll store it as member.
     private Rigidbody _rigidbody;
@@ -39,6 +45,80 @@ public class PlayerController : MonoBehaviour
         // grabbing this ensures we retain the gravity speed.
         Vector3 currentSpeed = _rigidbody.linearVelocity;
 
+        currentSpeed = PlayerMove(ref currentSpeed);
+
+        currentSpeed = PlayerJump(ref currentSpeed);
+
+        // store the current facing
+        // do this after the speed is adjusted by arrow keys.
+        // be before friction is applied.
+        if (currentSpeed.x != 0 && currentSpeed.z != 0)
+        {
+            _currentFacing = currentSpeed.normalized;
+        }
+
+        AdjustPlayerFriction(ref currentSpeed);
+
+        // fire the weapon?
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            GameObject newBullet = Instantiate(
+                _bulletToSpawn,
+                transform.position,
+                Quaternion.identity
+            );
+
+            Bullet bullet = newBullet.GetComponent<Bullet>();
+            if (bullet)
+            {
+                bullet.SetDirection(
+                    new Vector3(
+                        _currentFacing.x,
+                        0f,
+                        _currentFacing.z
+                    )
+                );
+            }
+        }
+
+        // apply max speed
+        currentSpeed.x = Mathf.Clamp(currentSpeed.x, movementVelocityMax * -1, movementVelocityMax);
+        currentSpeed.z = Mathf.Clamp(currentSpeed.z, movementVelocityMax * -1, movementVelocityMax);
+
+        _rigidbody.linearVelocity = currentSpeed;
+    }
+
+    private void AdjustPlayerFriction(ref Vector3 currentSpeed)
+    {
+        // if both left and right keys are simultaneously pressed (or not pressed), apply friction
+        if (Input.GetKey(KeyCode.LeftArrow) == Input.GetKey(KeyCode.RightArrow))
+        {
+            currentSpeed.x -= (movementDeceleration * currentSpeed.x);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) == Input.GetKey(KeyCode.DownArrow))
+        {
+            currentSpeed.z -= (movementDeceleration * currentSpeed.z);
+        }
+    }
+
+    private Vector3 PlayerJump(ref Vector3 currentSpeed)
+    {
+        // does player want to jump?
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(currentSpeed.y) < 1)
+        {
+            currentSpeed.y += jumpVelocity;
+        }
+        else
+        {
+            currentSpeed.y -= extraGravity * Time.deltaTime;
+        }
+
+        return currentSpeed;
+    }
+
+    private Vector3 PlayerMove(ref Vector3 currentSpeed)
+    {
         // check to see if any of the keyboard arrows are pressed.
         // if so, adjust the speed of the player.
         if (Input.GetKey(KeyCode.RightArrow))
@@ -61,32 +141,7 @@ public class PlayerController : MonoBehaviour
             currentSpeed.z -= (movementAcceleration * Time.deltaTime);
         }
 
-        // does player want to jump?
-        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(currentSpeed.y) < 1)
-        {
-            currentSpeed.y += jumpVelocity;
-        }
-        else
-        {
-            currentSpeed.y -= extraGravity * Time.deltaTime;
-        }
-
-        // if both left and right keys are simultaneously pressed (or not pressed), apply friction
-        if (Input.GetKey(KeyCode.LeftArrow) == Input.GetKey(KeyCode.RightArrow))
-        {
-            currentSpeed.x -= (movementDeceleration * currentSpeed.x);
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow) == Input.GetKey(KeyCode.DownArrow))
-        {
-            currentSpeed.z -= (movementDeceleration * currentSpeed.z);
-        }
-
-        // apply max speed
-        currentSpeed.x = Mathf.Clamp(currentSpeed.x, movementVelocityMax * -1, movementVelocityMax);
-        currentSpeed.z = Mathf.Clamp(currentSpeed.z, movementVelocityMax * -1, movementVelocityMax);
-
-        _rigidbody.linearVelocity = currentSpeed;
+        return currentSpeed;
     }
 
     private void OnTriggerEnter(Collider other)
