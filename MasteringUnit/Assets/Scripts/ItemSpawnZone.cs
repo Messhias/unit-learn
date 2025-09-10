9ecc3154da9cfc494b8bd202ad111a6a3f43b246
Fabio William Conceição <messhias@gmail.com>
2025-09-10 08:02:00 +0100

    Improve item spaw zone

--- MasteringUnit/Assets/Scripts/ItemSpawnZone.cs
+++ MasteringUnit/Assets/Scripts/ItemSpawnZone.cs
@@ -11,15 +11,67 @@ public class ItemSpawnZone : MonoBehaviour
     [SerializeField] [Tooltip("The area to spawn these items.")]
     private BoxCollider _spawnZone;
 
+    [SerializeField, Tooltip("How objects are organized when spawned.")]
+    private SpawnShape _spawnShape;
+
+    private enum SpawnShape
+    {
+        Random,
+        Circle,
+        Grid,
+        Count,
+    }
+
+    [SerializeField, Tooltip("Speed that this group of objects will rotate.")]
+    private Vector3 _rotationSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        for (var i = 0; i < _itemCount; i++) SpawnItemAtRandomPosition();
+        if (_spawnShape == SpawnShape.Circle)
+        {
+            SpawnObjectsInCircle();
+        }
+        else
+        {
+            for (var i = 0; i < _itemCount; i++) SpawnItemAtRandomPosition();
+        }
+    }
+
+    /// <summary>
+    /// Go through all the objectsand sapwn them in a circle.
+    /// Radius is determined by th size of the spawn zone collider.
+    /// </summary>
+    private void SpawnObjectsInCircle()
+    {
+        float radius = _spawnZone.bounds.size.x / 2;
+        Transform parent = this.gameObject.transform;
+        
+        for (int i = 0;i < _itemCount; i++)
+        {
+            // get the position on the circle to spawn this object.
+            float angle = i * Mathf.PI * 2 / _itemCount;
+            Vector3 position = Vector3.zero;
+            position.x = Mathf.Cos(angle);
+            position.z = Mathf.Sin(angle);
+            position *= radius;
+            position += _spawnZone.bounds.center;
+            
+            // spawn as a child of the parent object.
+            GameObject newObject = Instantiate(_itemToSpawn, parent);
+            newObject.transform.localPosition = position;
+        }
     }
 
     // Update is called once per frame
     private void Update()
     {
+        // calculate the new rotation.
+        // by taking the old rotation.
+        // and applying the speed parameter.
+        Vector3 newRot =  transform.eulerAngles;
+        newRot += _rotationSpeed * Time.deltaTime;
+        transform.localEulerAngles = newRot;
     }
 
     private void SpawnItemAtRandomPosition()
