0a3a3744986e95f513284cc9fcfb025aeaacca45
Fabio William Conceição <messhias@gmail.com>
2025-09-21 18:45:16 +0100

    Created immutable list implementation.

--- MasteringUnit/Assets/Scripts/Weapon.cs
+++ MasteringUnit/Assets/Scripts/Weapon.cs
@@ -1,10 +1,27 @@
 using Base;
+using Contracts;
 using UnityEngine;
 
 public class Weapon : WeaponBase
 {
     public override void OnAttack(Vector3 facing)
     {
+        if (_bulletToSpawn)
+        {
+            var newBullet = Instantiate(
+                _bulletToSpawn,
+                transform.position,
+                Quaternion.identity
+            );
+            
+            IBullet bullet =  newBullet.GetComponent<Bullet>();
+
+            bullet?.SetDirection(new Vector3(facing.x, 0f, facing.z));
+
+            return;
+        }
+        
+        
         transform.position += facing;
         transform.Rotate(new Vector3(45f, -100f, 60f));
         PauseMovementTimer = PauseMovementMax;
