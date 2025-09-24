fae85b164581db3969c47b009c9e55c17bad9c6b
Fabio William Conceição <messhias@gmail.com>
2025-09-24 09:18:17 +0100

    Bug fixes

--- MasteringUnit/Assets/Scripts/Bullet.cs
+++ MasteringUnit/Assets/Scripts/Bullet.cs
@@ -1,18 +1,20 @@
 using System.Collections.Generic;
-using System.Linq;
 using Contracts;
 using UnityEngine;
+using System.Linq;
 
 public class Bullet : MonoBehaviour, IBullet
 {
-    #region *** private properties ***
+    [SerializeField] [Tooltip("Speed of this bullet.")]
+    private float _speed = 4f;
+
+    [SerializeField] [Tooltip("Normalized direction of this bullet.")]
+    private Vector3 _direction = Vector3.zero;
 
     private readonly IReadOnlyCollection<string> _canDestroy = new[]
     {
         "EnemyObj_Spikes"
     };
-
-    #endregion
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
@@ -22,66 +24,47 @@ public class Bullet : MonoBehaviour, IBullet
     // Update is called once per frame
     private void Update()
     {
-        if (bulletDirection.sqrMagnitude > 0f) transform.position += bulletDirection * (speed * Time.deltaTime);
+        if (_direction.sqrMagnitude > 0f) transform.position += _direction * (_speed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision other)
     {
         var target = other.gameObject;
+
+        if (!_canDestroy.Any(item => target.name.Contains(item))) return;
+        
         Debug.unityLogger.Log("Bullet hit " + target);
-
-        if (!_canDestroy.Contains(target.name)) return;
-
+        
         IVFXHandler vfxHandler = target.GetComponent<VFXHandler>();
         vfxHandler?.SpawnExplosion();
-
-        // apply knockback when damage is dealt
-        var rb = other.gameObject?.GetComponent<Rigidbody>();
-        if (rb != null)
-            rb.AddExplosionForce(100, transform.position, 10f);
-
         Destroy(target);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         var target = other.gameObject;
+
+        if (!_canDestroy.Any(item => target.name.Contains(item))) return;
+        
         Debug.unityLogger.Log("Bullet hit " + target);
-
-        if (!_canDestroy.Contains(target.name)) return;
-
+        
         IVFXHandler vfxHandler = target.GetComponent<VFXHandler>();
         vfxHandler?.SpawnExplosion();
-
-        // apply knockback when damage is dealt
-        var rb = other.gameObject?.GetComponent<Rigidbody>();
-        if (rb != null)
-            rb.AddExplosionForce(100, transform.position, 10f);
 
         Destroy(target);
     }
 
     public void SetDirection(Vector3 direction)
     {
-        // bulletDirection.y = 0f;
+        // _direction.y = 0f;
 
         if (direction.sqrMagnitude <= Mathf.Epsilon)
             return;
 
-        bulletDirection = direction.normalized;
+        _direction = direction.normalized;
 
-        transform.rotation = Quaternion.LookRotation(bulletDirection, Vector3.up);
+        transform.rotation = Quaternion.LookRotation(_direction, Vector3.up);
 
-        transform.LookAt(transform.position + bulletDirection);
+        transform.LookAt(transform.position + _direction);
     }
-
-    #region *** Editor fields ***
-
-    [SerializeField] [Tooltip("Speed of this bullet.")]
-    private float speed = 4f;
-
-    [SerializeField] [Tooltip("Normalized direction of this bullet.")]
-    private Vector3 bulletDirection = Vector3.zero;
-
-    #endregion
 }
